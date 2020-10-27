1dd3cc39acb4b14a89519319973ccf57428d080c
Charath Ranganathan <444119+charathram@users.noreply.github.com>
2020-10-27 11:37:53 -0700

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -1,6 +1,6 @@
 export const installed_blueprints = [
   //@BlueprintInsertion
-
+{ name: 'SignupScreen313266', human_name: 'Signup Screen', access_route: 'SignupScreen313266'},
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
